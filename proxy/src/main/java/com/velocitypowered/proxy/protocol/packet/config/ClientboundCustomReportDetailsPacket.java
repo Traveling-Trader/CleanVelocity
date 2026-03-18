5ca1a7bdec076a6276c21b671dfc4c011eb9dbb8
Traveling-Trader <132439817+Traveling-Trader@users.noreply.github.com>
2026-03-18 17:38:20 +0200

    Add more protocol safeguards (#1743)
    * Add max length to more config phase packets
    
    * Ensure all packets during non-play state are known
    
    * Limit maximum size of play inbound packet queue
    
    * Fix checkstyle

--- proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/config/ClientboundCustomReportDetailsPacket.java
+++ proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/config/ClientboundCustomReportDetailsPacket.java
@@ -22,6 +22,7 @@ import com.velocitypowered.proxy.connection.MinecraftSessionHandler;
 import com.velocitypowered.proxy.protocol.MinecraftPacket;
 import com.velocitypowered.proxy.protocol.ProtocolUtils;
 import io.netty.buffer.ByteBuf;
+
 import java.util.HashMap;
 import java.util.Map;
 
